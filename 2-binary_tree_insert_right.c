1713d4bb131ccbf701f405d5b6cff81a575f905d
Wandile Mawelela <wandile1997@gmail.com>
2024-01-05 03:18:46 +0200

    [FIX] Hot fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,12 +11,14 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
+
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
 
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
 	{
